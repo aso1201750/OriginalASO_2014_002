c2d2fd9db3e70b916a1a62490b3dd7079b7e1242
aso1201750 <1201750@st.asojuku.ac.jp>
2014-07-28 20:42:17 +0900

    人間2

--- src/jp/ac/st/asojuku/original2014002/HitokotoActivity.java
+++ src/jp/ac/st/asojuku/original2014002/HitokotoActivity.java
@@ -1,8 +1,9 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-
+import android.widget.TextView;
 public class HitokotoActivity extends Activity {
 
 	@Override
@@ -10,6 +11,24 @@ public class HitokotoActivity extends Activity {
 		// TODO 自動生成されたメソッド・スタブ
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.hitokoto_activity);
+	}
+
+
+
+
+	@Override
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		// 画面 (Activity)に渡されたインテントを取得
+		Intent intent = this.getIntent();
+		// intentから、Extraに混入させた値をキーワードで取得
+		String strHitokoto = intent.getStringExtra("hitokoto");
+
+		// 取得したStringを、TxtViewにセット
+		TextView txvHITOKOTO = (TextView)findViewById(R.id.textView1);
+		txvHITOKOTO.setText(strHitokoto);
 	}
 
 }
--- src/jp/ac/st/asojuku/original2014002/MainActivity.java
+++ src/jp/ac/st/asojuku/original2014002/MainActivity.java
@@ -1,64 +1,106 @@
 package jp.ac.st.asojuku.original2014002;
 
 import android.app.Activity;
-import android.app.ActionBar;
-import android.app.Fragment;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteException;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Button;
+import android.widget.EditText;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
 
+	SQLiteDatabase sdb = null;
+	MySQLiteOpenHelper helper = null;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
-		if (savedInstanceState == null) {
-			getFragmentManager().beginTransaction()
-					.add(R.id.container, new PlaceholderFragment())
-					.commit();
-		}
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
 
 	@Override
-	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+	protected void onResume() {
+		// TODO 自動生成されたメソッド・スタブ
+		super.onResume();
+
+		// 登録ボタン変数にリスナーを登録する
+		Button btnENTRY = (Button)findViewById(R.id.btnENTRY);
+		btnENTRY.setOnClickListener(this);
+
+		// メンテボタン変数にリスナーを登録
+		Button btnMAINTE = (Button)findViewById(R.id.btnMAINTE);
+		btnMAINTE.setOnClickListener(this);
+
+		//一言チェックボタン変数にリスナーを登録する
+		Button btnCHECK = (Button)findViewById(R.id.btnCHECK);
+		btnCHECK.setOnClickListener(this);
+
+		//クラスのフィールド変数がNULLなら、データベース空間オープン
+		if(sdb == null) {
+			helper = new MySQLiteOpenHelper(getApplicationContext());
 		}
-		return super.onOptionsItemSelected(item);
+		try{
+			sdb = helper.getWritableDatabase();
+		}catch(SQLiteException e){
+			//以上終了
+			return;
+		}
+
+	}
+	@Override
+	public void onClick(View v) {
+		//TODO 自動生成されたメソッド・スタブ
+
+		// 生成して代入用のIntentインスタンス変数を用意
+		Intent intent = null;
+		switch(v.getId()) { //どのボタンが押されたか判定
+
+		case R.id.btn_DELETE://登録ボタンが押された
+			//エディットテキストからの入力内容を取り出す
+			EditText etv = (EditText)findViewById(R.id.EditText);
+			String inputMsg = etv.getText().toString();
+
+			// inputMsgがnullでない、かつ、空でない場合のみ、if文内を実行
+			if(inputMsg!=null && ! inputMsg.isEmpty()){
+				// MySQLiteOperHelperのインサートメソッドを呼び出し
+				helper.insertHitokoto(sdb, inputMsg);
+			}
+
+			// 入力欄をクリア
+			etv.setText("");
+			break;
+		case R.id.btnMAINTE_BACK: //メンテボタンが押された
+
+			//インテントのインスタンス生成
+			intent = new Intent(MainActivity.this, MaintenanceActivity.class);
+			//次画面のアクティビティ起動
+			startActivity(intent);
+			break;
+		case R.id.btnCHECK: //一言チェックボタンが押された
+
+			//MySQLiteOpenHelperのセレクト一言メソッドを呼び出してひとこと
+			String strHitokoto = helper.selectRandomHitokotoActivity(sdb);
+
+			//インテントのインスタンス生成
+			intent = new Intent(MainActivity.this, HitokotoActivity.class);
+			// インテントに一言を混入
+			intent.putExtra("hitokoto", strHitokoto);
+
+			// 次画面のアクティビティ起動
+			startActivity(intent);
+			break;
+
+		}
 	}
 
-	/**
-	 * A placeholder fragment containing a simple view.
-	 */
-	public static class PlaceholderFragment extends Fragment {
-
-		public PlaceholderFragment() {
-		}
-
-		@Override
-		public View onCreateView(LayoutInflater inflater, ViewGroup container,
-				Bundle savedInstanceState) {
-			View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-			return rootView;
-		}
-	}
 
 }
